bd1ae0b57e73d2ee30cd53781218bfe205ceaf82
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2020-02-07 12:08:06 +0545

    add mixup loss and cumix loss during training

--- augmentation.py
+++ augmentation.py
@@ -74,3 +74,21 @@ def mixup_criterion(preds1,preds2,preds3, targets, rate=0.7):
     # criterion = nn.CrossEntropyLoss(reduction='mean')
     criterion = ohem_loss
     return [ lam * criterion(rate, preds1, targets1) + (1 - lam) * criterion(rate, preds1, targets2), lam * criterion(rate, preds2, targets3) + (1 - lam) * criterion(rate, preds2, targets4), lam * criterion(rate, preds3, targets5) + (1 - lam) * criterion(rate, preds3, targets6) ]
+
+
+
+for i, (image_id, images, label1, label2, label3) in enumerate(data_loader_train):
+    images = images.to(device)
+    label1 = label1.to(device)
+    label2 = label2.to(device)
+    label3 = label3.to(device)
+    # print (image_id, label1, label2, label3)
+
+    if np.random.rand()<0.5:
+        images, targets = mixup(images, label1, label2, label3, 0.4)
+        output1, output2, output3 = model(images)
+        loss = mixup_criterion(output1,output2,output3, targets) 
+    else:
+        images, targets = cutmix(images, label1, label2, label3, 0.4)
+        output1, output2, output3 = model(images)
+        loss = cutmix_criterion(output1,output2,output3, targets) 
